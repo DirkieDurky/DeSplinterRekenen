d16decb910edc67018423e14edf02ababcf6909e
DirkieDurky <dirk@freijters.nl>
2021-02-22 17:09:06 +0100

    niks

--- Code/Teacher/AssignmentEditor/uploadQuestionImage.php
+++ Code/Teacher/AssignmentEditor/uploadQuestionImage.php
@@ -2,9 +2,15 @@
 require_once "../../DB_Connection.php";
 session_start();
 
-$file = basename($_FILES["fileToUpload"]["name"]);
+//    if ($_FILES["fileToUpload"]["size"] > 500000) {
+//        $_SESSION['error'] = "Sorry, de maximale bestandsgrootte is 5mb";
+//    }
+//    $imageFileType = pathinfo($_FILES['fileToUpload']['name'])['extension'];
+//    if ($imageFileType != "png" && $imageFileType != "jpg" && $imageFileType != "jpeg" && $imageFileType != "gif") {
+//        $_SESSION['error'] = "Sorry, je kunt alleen PNG, JPG, JPEG, of GIF bestanden gebruiken.";
+//    }
+    $file = basename($_FILES["fileToUpload"]["name"]);
 
-require_once "../../DB_Connection.php";
     $sth = $pdo -> prepare("SELECT `media` FROM questions WHERE assignmentID = ? AND `order` = ?");
     $sth -> execute([$_SESSION['editingAssign'], $_SESSION['editingQuestion']]);
     $row = $sth -> fetch();
@@ -24,14 +30,15 @@ require_once "../../DB_Connection.php";
     $number = intval(substr($files[0], 0, $underscoreLocation))+1 . "_";
     $location = $targetDir . $number . $fileName . $ext;
 
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$location)) {
-        $sth2 = $pdo -> prepare("UPDATE `questions` SET media = ? WHERE assignmentID = ? AND `order` = ?");
-        $sth2 -> execute([$location,$_SESSION['editingAssign'], $_SESSION['editingQuestion']]);
-
-        $_SESSION['notification'] = "Afbeelding succesvol geupload!";
-    } else {
-        $_SESSION['notification'] = "Er ging iets mis bij het uploaden van je afbeelding.";
-    }
+    move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$location);
+//    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$location)) {
+//        $sth2 = $pdo -> prepare("UPDATE `questions` SET media = ? WHERE assignmentID = ? AND `order` = ?");
+//        $sth2 -> execute([$location,$_SESSION['editingAssign'], $_SESSION['editingQuestion']]);
+//
+//        $_SESSION['notification'] = "Afbeelding succesvol geupload!";
+//    } else {
+//        $_SESSION['error'] = "Sorry, er ging iets mis bij het uploaden van je afbeelding.";
+//    }
 
 header("Location: assignmentEditor.php?assign=" . $_SESSION['editingAssign'] . "&question=" . $_SESSION['editingQuestion']);
 exit();
